d98ebe68d2e778d730547d6b3c241b8b1333e2c7
DoubyBear <38573771+nicedouby@users.noreply.github.com>
2026-07-26 20:19:26 +0800

    Fix stepwise policy contract assertion

--- app/tests/run-stepwise-squad-policy-integration-tests.js
+++ app/tests/run-stepwise-squad-policy-integration-tests.js
@@ -9,7 +9,7 @@ const source = await fs.readFile(path.join(root, "app/plugins/stepwise-squad-pla
 assert.match(source, /classifySquadNameWithPolicy/);
 assert.match(source, /classification\?\.source/);
 assert.match(source, /classification_missing/);
-assert.doesNotMatch(source, /classifySquadName\(/);
+assert.doesNotMatch(source, /domain\\/squad\\/squad_name_classifier/);
 assert.doesNotMatch(source, /domain\/squad\/squad_name_classifier/);
 
 console.log("stepwise squad policy integration tests passed");
